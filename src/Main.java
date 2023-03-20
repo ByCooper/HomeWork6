fb05d86f78dbfa8b1c7f6132a03bca2b22468253
ByCooper <kup.official@yahoo.com>
2023-03-20 17:24:16 +0300

    ДЗ №6 (Начало)

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача №1" + "\n");
+
+        for (int i = 1; i <= 10; i++)
+        System.out.println("Iteration " + i);
+    }
+    public static void task2 () {
+
+    }
+}
